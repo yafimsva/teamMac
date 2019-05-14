36bb131b6401626e62fc658f6941704f8d182689
Brandon Skar <bransk01@yahoo.com>
2019-05-14 16:53:18 -0400

    Created Unit Testing class

---
+++ tdd/databaseTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brandon
+ * Date: 5/14/2019
+ * Time: 1:44 PM
+ */
+
+use PHPUnit\Framework\TestCase as TestCase;
+
+class DatabaseTest extends TestCase
+{
+
+}
