386e16fb88a01f22542c6d154c9b72c644ca7df1
Brandon Skar <bransk01@yahoo.com>
2019-05-27 16:46:49 -0400

    Able to upload files to the files directory for admins (David, Brandon)

--- index.php
+++ index.php
@@ -260,6 +260,58 @@ $f3->route('GET|POST /file', function ($f3) {
 	$template = new Template();
 	echo $template->render('files.html');
 });
+
+$f3->route('GET|POST /upload', function($f3) {
+    $target_dir = "files/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    if(isset($_POST['submit'])) {
+//        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//        if($check !== false) {
+//            echo "File is an image - " . $check["mime"] . ".";
+//            $uploadOk = 1;
+//        }
+//        else {
+//            echo "File is not an image.";
+//            $uploadOk = 0;
+//        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+//        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//            && $imageFileType != "gif" ) {
+//            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//            $uploadOk = 0;
+//        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                print_r($_POST);
+                $template = new Template();
+                echo $template->render('views/admin.html');
+            } else {
+                echo "<h1>SORRY</h1>";
+            }
+        }
+    }
+});
+
 $f3->route('GET|POST /logout', function ($f3) {
 	session_destroy();
 	$f3->reroute('/');
