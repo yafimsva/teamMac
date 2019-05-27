5d3658c8202aa92b90747c0189e5b0a96300bc9b
Yafim Vainilovich <yafimsva@gmail.com>
2019-05-27 02:17:40 -0700

    PULL THIS

--- index.php
+++ index.php
@@ -1,103 +1,65 @@
 <?php
-
 /*
  * Yafim Vainilovich
  * April 23, 2019
  * 355/teamMac/index.php
  */
-
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
 //Require autoload
 require_once('vendor/autoload.php');
 require_once('model/database.php');
 session_start();
-
 //Create an instance of the Base class
 $f3 = Base::instance();
-
 //Turn of Fat-Free error reporting
 $f3->set('DEBUG', 3);
-
 $f3->route('GET|POST /home', function ($f3) {
 	print_r($_POST);
 	if (!isset($_SESSION['teacherLogin'])) {
 		$f3->reroute('/');
 	}
-
 	$db = new Database();
 	$db->connect();
-
 	$f3->set("currentDate", date("Y-m-d"));
-
 	$students = $db->getStudentsByID($_SESSION['classid']);
 	$dates = $db->getDates();
 	$attendances = $db->viewAttendance($_SESSION['classid']);
-
-
 	$f3->set('students', $students);
 	$f3->set('datesArray', $dates);
 	$f3->set('attendances', $attendances);
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-	foreach ($dates as $date) {
-		array_push($finalDates, $date['date']);
-		array_push($finalStudents, $db->viewAttendanceByDateAndClassID($date['date'], $_SESSION['classid']));
-	}
-
-	$f3->set('dates', $finalDates);
-	$f3->set('allStudents', $finalStudents);
-
-	// print_r($finalDates);
-	print_r($finalStudents);
-	if(isset($_POST['attendance']))
-	{
-=======
+	// if (isset($_POST['students'])) {
+	// 	$_SESSION['students'] = $_POST['students'];
+	// 	$array = $_POST['students'];
+	// 	foreach ($students as $student) {
+	// 		if ($db->getAttendanceCount($_POST['date'], $student['sid']) == 0) {
+	// 			$db->takeAttendance($_POST['date'], $student['sid'], false);
+	// 		}
+	// 	}
+	// 	foreach ($array as $sid) {
+	// 		// if ($db->getAttendance($_POST['date'], $sid, true) == 0) {
+	// 		$db->updateAttendance($sid, true, $_POST['date']);
+	// 		// }
+	// 	}
+	// 	$f3->reroute('home#');
+	// }
 	if (isset($_POST['attendance'])) {
->>>>>>> 03f8812d00a667895383a28126b7da594626bf9d
 		$duplicate = false;
-
 		foreach ($_POST['attendance'] as $sid) {
 			$check = $db->checkIfAttedanceTaken($_POST['date'], $sid);
-
 			if (sizeof($check) == 0) {
 				$duplicate = false;
 			} else {
 				$duplicate = true;
 			}
 		}
-
-
-<<<<<<< HEAD
-		if(!$duplicate)
-		{
-			foreach ($students as $student)
-			{
-				$db->takeAttendance($_POST['date'], $student['sid'], 0, $_SESSION['classid']);
-=======
 		if (!$duplicate) {
 			foreach ($students as $student) {
 				$db->takeAttendance($_POST['date'], $student['sid'], 0);
->>>>>>> 03f8812d00a667895383a28126b7da594626bf9d
 			}
-
 			foreach ($_POST['attendance'] as $sid) {
-<<<<<<< HEAD
-				$db->updateAttendance($sid, 1, $_POST['date'], $_SESSION['classid']);
-	
-=======
 				$db->updateAttendance($sid, 1, $_POST['date']);
-
->>>>>>> 03f8812d00a667895383a28126b7da594626bf9d
 				// $taken = $db->checkIfAttedanceTaken($_POST['date'], $sid);
 				// if(sizeof($taken) == 0)
 				// {
@@ -108,36 +70,27 @@ $f3->route('GET|POST /home', function ($f3) {
 				// 	echo("<script>alert('ERROR: Duplicate attendance entires')</script>");
 				// }
 			}
-
 			$f3->reroute('home');
 		} else {
 			$f3->reroute('home#calendar');
 		}
 	}
-
-
 	$template = new Template();
 	echo $template->render('views/index.html');
 });
-
 $f3->route('GET|POST /', function ($f3) {
 	$db = new Database();
 	$db->connect();
-
 	if (isset($_SESSION['teacherLogin'])) {
 		$f3->reroute('home');
 	} else if (isset($_SESSION['adminLogin'])) {
 		$f3->reroute('admin');
 	}
-
 	if (isset($_POST['username'], $_POST['password'])) {
-
 		if (strtolower($_POST['username']) == "admin" && $_POST['password'] == "admin") {
 			$_SESSION['adminLogin'] = true;
-
 			$f3->reroute('admin');
 		}
-
 		$results = $db->checkLogin($_POST['username'], $_POST['password']);
 		if (isset($results['teacherid'])) {
 			$_SESSION['teacherLogin'] = true;
@@ -147,44 +100,29 @@ $f3->route('GET|POST /', function ($f3) {
 			$_SESSION['classid'] = $results['classid'];
 			$_SESSION['class'] = $results['className'];
 			$_SESSION['daysLeft'] = $results['daysLeft'];
-
-
 			$f3->reroute('home');
 		} else {
 			$f3->reroute('/');
 		}
 		print_r($_SESSION);
 	}
-
-
 	$template = new Template();
 	echo $template->render('views/login.html');
 });
-
 $f3->route('GET|POST /admin', function ($f3) {
 	if (!isset($_SESSION['adminLogin'])) {
 		$f3->reroute('/');
 	}
 	$db = new Database();
 	$db->connect();
-
 	$students = $db->getStudents();
 	$teachers = $db->getTeachers();
 	$classes = $db->getClasses();
 	$helpers = $db->getHelpers();
-
-
 	$f3->set('students', $students);
 	$f3->set('teachers', $teachers);
 	$f3->set('classes', $classes);
 	$f3->set('helpers', $helpers);
-
-
-
-
-
-
-
 	// post student into database 
 	if (isset($_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['dob'])) {
 		if ($_POST['studentClass'] != 0) {
@@ -193,14 +131,12 @@ $f3->route('GET|POST /admin', function ($f3) {
 			$email = $_POST['email'];
 			$dob = $_POST['dob'];
 			$studentClass = $_POST['studentClass'];
-
 			$db->insertStudent($firstName, $lastName, $dob, $email, $studentClass);
 			$f3->reroute('admin#students');
 		} else {
 			$f3->set("errors['studentClass']", "You did not pick a class for your student, student was not added");
 		}
 	}
-
 	// post teacher into database 
 	if (isset($_POST['teacherName'], $_POST['teacherUsername'], $_POST['password'], $_POST['confirmPassword'], $_POST['teacherClass'])) {
 		if ($_POST['password'] == $_POST['confirmPassword']) {
@@ -223,14 +159,11 @@ $f3->route('GET|POST /admin', function ($f3) {
 			$f3->set("errors['nomatch']", "Passwords did not match, teacher was not added");
 		}
 	}
-
-
 	//post classes into database
 	if ($_POST['className']) {
 		$db->insertClass($_POST['className']);
 		$f3->reroute('admin#classes');
 	}
-
 	// post student into database 
 	if (isset($_POST['updateFirstName'], $_POST['updateLastName'], $_POST['updateEmail'], $_POST['updateDob'], $_POST['updateStudentClass'])) {
 		$updateFirstName = $_POST['updateFirstName'];
@@ -239,17 +172,14 @@ $f3->route('GET|POST /admin', function ($f3) {
 		$updateDob = $_POST['updateDob'];
 		$updateStudentClass = $_POST['updateStudentClass'];
 		$sid = $_POST['sid'];
-
 		$db->updateStudent($sid, $updateFirstName, $updateLastName, $updateDob, $updateEmail, $updateStudentClass);
 		$f3->reroute('admin#students');
 	}
-
 	//deletes a student
 	if (isset($_POST['deleteStudent'])) {
 		$db->deleteStudent($_POST['deleteStudent']);
 		$f3->reroute('admin#students');
 	}
-
 	//updates a class name
 	if (isset($_POST['updateClassName'], $_POST['updateClassNameID'])) {
 		$newClassName = $_POST['updateClassName'];
@@ -257,7 +187,6 @@ $f3->route('GET|POST /admin', function ($f3) {
 		$db->updateClass($newClassName, $updateClassNameID);
 		$f3->reroute('admin#classes');
 	}
-
 	//updates a teacher 
 	if (isset($_POST['updateTeacherName'], $_POST['updateTeacherUsername'])) {
 		if ($_POST['updatePassword'] == $_POST['updateConfirmPassword']) {
@@ -272,7 +201,6 @@ $f3->route('GET|POST /admin', function ($f3) {
 			$updateTeacherClass = $_POST['updateTeacherClass'];
 			$updateTeacherId = $_POST['teacherid'];
 			$db->updateTeacher($updateTeacherId, $updateTeacherName, $updateTeacherUsername, $updatePassword, $updateTeacherClass, $updateTeacherEndDate);
-
 			if (isset($_POST['onStudentsPage'])) {
 				$f3->reroute('admin#students');
 			} else {
@@ -282,24 +210,19 @@ $f3->route('GET|POST /admin', function ($f3) {
 			$f3->set("errors['updateNoMatch']", "Passwords did not match, teacher was not updated");
 		}
 	}
-
 	//deletes a student
 	if (isset($_POST['deleteTeacher'])) {
 		$db->deleteTeacher($_POST['deleteTeacher']);
-
 		if (isset($_POST['onStudentsPage'])) {
 			$f3->reroute('admin#students');
 		} else {
 			$f3->reroute('admin#teachers');
 		}
 	}
-
 	//insert a helper
 	if (isset($_POST['helperName'], $_POST['helperClass'])) {
-
 		if ($_POST['helperClass'] != 0) {
 			$db->insertHelper($_POST['helperName'], $_POST['helperClass']);
-
 			if (isset($_POST['onStudentsPage'])) {
 				$f3->reroute('admin#students');
 			} else {
@@ -309,54 +232,39 @@ $f3->route('GET|POST /admin', function ($f3) {
 			$f3->set("errors['noClassHelper']", "You did not pick a class for your helper, helper was not added");
 		}
 	}
-
 	//updates a helper 
 	if (isset($_POST['updateHelperName'], $_POST['updateHelperClass'])) {
-
 		$updateHelperName = $_POST['updateHelperName'];
 		$updateHelperClass = $_POST['updateHelperClass'];
 		$helperid = $_POST['helperid'];
-
 		$db->updateHelper($updateHelperName, $updateHelperClass, $helperid);
-
 		if (isset($_POST['onStudentsPage'])) {
 			$f3->reroute('admin#students');
 		} else {
 			$f3->reroute('admin#teachers');
 		}
 	}
-
 	//deletes a helper
 	if (isset($_POST['deleteHelper'])) {
 		$db->deleteHelper($_POST['deleteHelper']);
-
 		if (isset($_POST['onStudentsPage'])) {
 			$f3->reroute('admin#students');
 		} else {
 			$f3->reroute('admin#teachers');
 		}
 	}
-
-
 	$template = new Template();
 	echo $template->render('views/admin.html');
 });
-
 $f3->route('GET|POST /file', function ($f3) {
-
 	$template = new Template();
 	echo $template->render('files.html');
 });
-
 $f3->route('GET|POST /logout', function ($f3) {
 	session_destroy();
 	$f3->reroute('/');
-
 	$template = new Template();
 	echo $template->render('views/login.html');
 });
-
-
-
 //Run fat free
 $f3->run();
--- model/database.php
+++ model/database.php
@@ -1,6 +1,4 @@
 <?php
-
-
 if ($_SERVER['USER'] == 'yvainilo') {
     require_once('/home/yvainilo/config.php');
 } else if ($_SERVER['USER'] == 'bskargre') {
@@ -10,13 +8,11 @@ if ($_SERVER['USER'] == 'yvainilo') {
 } else if ($_SERVER['USER'] == 'dkovalev') {
     require_once '/home/dkovalev/config.php';
 }
-
 /**
  * Class Database database class to insert and get information
  */
 class Database
 {
-
     public function connect()
     {
         try {
@@ -26,48 +22,37 @@ class Database
             echo $e->getMessage();
         }
     }
-
     function insertStudent($first, $last, $dob, $parents_email, $studentClass)
     {
         global $dbh;
-
         $sql = "INSERT INTO students (first, last, dob, parents_email, classid)
             VALUES(:fname, :lname, :dob, :parents_email, :classid);";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':fname', $first, PDO::PARAM_STR);
         $statement->bindValue(':lname', $last, PDO::PARAM_STR);
         $statement->bindValue(':dob', $dob, PDO::PARAM_STR);
         $statement->bindValue(':parents_email', $parents_email, PDO::PARAM_STR);
         $statement->bindValue(':classid', $studentClass, PDO::PARAM_INT);
-
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     public function getStudents()
     {
         global $dbh;
-
         $sql = "SELECT students.*, classes.className
         FROM students
         INNER JOIN classes
         ON students.classid = classes.classid 
         ORDER BY classid ASC, last ASC";
-
         $statement = $dbh->prepare($sql);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
         //
         //        $posts = array();
@@ -77,113 +62,54 @@ class Database
         //            $first = $result['first'];
         //            $last = $result['last'];
         //        }
-
         return $results;
     }
-
     public function getStudentsByID($classid)
     {
         global $dbh;
-
         $sql = "SELECT * FROM `students` WHERE classid = :classid ORDER BY last ASC;";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
-    function takeAttendance($date, $sid, $present, $class)
+    function takeAttendance($date, $sid, $present)
     {
         global $dbh;
-
-        $sql = "INSERT INTO attendance (date, sid, present, class)
-            VALUES(:date, :sid, :present, :class);";
-
+        $sql = "INSERT INTO attendance (date, sid, present)
+            VALUES(:date, :sid, :present);";
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':date', $date, PDO::PARAM_STR);
         $statement->bindValue(':sid', $sid, PDO::PARAM_STR);
         $statement->bindValue(':present', $present, PDO::PARAM_STR);
-        $statement->bindValue(':class', $class, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
-    function updateAttendance($sid, $present, $date, $class)
+    function updateAttendance($sid, $present, $date)
     {
         global $dbh;
-
-        $sql = "UPDATE attendance SET present = :present WHERE sid = :sid AND date = :date AND class = :class;";
-
-
+        $sql = "UPDATE attendance SET present = :present WHERE sid = :sid AND date = :date;";
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':date', $date, PDO::PARAM_STR);
         $statement->bindValue(':sid', $sid, PDO::PARAM_STR);
         $statement->bindValue(':present', $present, PDO::PARAM_STR);
-        $statement->bindValue(':class', $class, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
-<<<<<<< HEAD
-
-
-
-    //////////////// DAVIDS ATTENDANCE CODE ////////////////
-
-    function attendanceByClassID($classid)
-    {
-        global $dbh;
-
-        $sql = "SELECT * FROM `attendance` WHERE class = :classid;";
-
-        $statement = $dbh->prepare($sql);
-
-        $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
-
-        $statement->execute();
-        $arr = $statement->errorInfo();
-        if (isset($arr[2])) {
-            print_r($arr[2]);
-        }
-
-        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-        return $results;
-    }
-
-
-
-    //////////////// END DAVIDS ATTENDANCE CODE END ////////////////
-
-
-
-    public function viewAttendanceByDateAndClassID($date, $classid)
-=======
     public function viewAttendance($classid)
->>>>>>> 03f8812d00a667895383a28126b7da594626bf9d
     {
         global $dbh;
-
         $sql = "SELECT students.first, students.last,
         attendance.date, attendance.sid, attendance.present, classes.classid
         FROM students
@@ -192,362 +118,261 @@ class Database
         INNER JOIN classes 
         ON students.classid = classes.classid
         WHERE students.classid = :classid;";
-
         $statement = $dbh->prepare($sql);
         $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
-
         $statement->execute();
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
     public function getAttendanceCount($date, $sid)
     {
         global $dbh;
-
         $sql = "SELECT date, sid FROM attendance WHERE date = :date AND sid = :sid";
-
         $statement = $dbh->prepare($sql);
         $statement->bindValue(':date', $date, PDO::PARAM_STR);
         $statement->bindValue(':sid', $sid, PDO::PARAM_STR);
         // $statement->bindValue(':present', $present, PDO::PARAM_STR);
         $statement->execute();
         $results = $statement->rowCount();
-
         return $results;
     }
-
     public function getDates()
     {
         global $dbh;
-
         $sql = "SELECT DISTINCT date FROM attendance ORDER BY date DESC";
-
         $statement = $dbh->prepare($sql);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
     function insertTeacher($teacherName, $teacherUsername, $teacherPassword, $teacherClass, $endDate)
     {
         global $dbh;
-
         $sql = "INSERT INTO teachers (name, username, password, classid, endDate)
             VALUES(:name, :username, :password, :classid, :endDate);";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':name', $teacherName, PDO::PARAM_STR);
         $statement->bindValue(':username', $teacherUsername, PDO::PARAM_STR);
         $statement->bindValue(':password', $teacherPassword, PDO::PARAM_STR);
         $statement->bindValue(':classid', $teacherClass, PDO::PARAM_STR);
         $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
-
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function insertClass($className)
     {
         global $dbh;
-
         $sql = "INSERT INTO classes (className)
             VALUES(:className);";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':className', $className, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function insertHelper($helperName, $classid)
     {
         global $dbh;
-
         $sql = "INSERT INTO helpers(name, classid) 
                 VALUES (:helperName, :classid)";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':helperName', $helperName, PDO::PARAM_STR);
         $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
-
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     public function getHelpers()
     {
         global $dbh;
-
         $sql = "SELECT helpers.*, classes.className
         FROM helpers
         INNER JOIN classes
         ON helpers.classid = classes.classid 
         ORDER BY name ASC;";
-
         $statement = $dbh->prepare($sql);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
-
     public function getTeachers()
     {
         global $dbh;
-
         $sql = "SELECT DATEDIFF(teachers.endDate ,CURRENT_DATE()) daysLeft, teachers.*, classes.className
         FROM teachers
         INNER JOIN classes
         ON teachers.classid = classes.classid 
         ORDER BY name ASC";
-
         $statement = $dbh->prepare($sql);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
-
-
     public function getClasses()
     {
         global $dbh;
-
         $sql = "SELECT * FROM classes SORT ORDER BY className ASC";
-
         $statement = $dbh->prepare($sql);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
         return $results;
     }
-
     function updateStudent($sid, $first, $last, $dob, $parents_email, $studentClass)
     {
         global $dbh;
-
         $sql = "UPDATE students 
         SET first = :fname, last = :lname, dob = :dob, parents_email = :parents_email, classid = :classid 
         WHERE sid = :sid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':fname', $first, PDO::PARAM_STR);
         $statement->bindValue(':lname', $last, PDO::PARAM_STR);
         $statement->bindValue(':dob', $dob, PDO::PARAM_STR);
         $statement->bindValue(':parents_email', $parents_email, PDO::PARAM_STR);
         $statement->bindValue(':classid', $studentClass, PDO::PARAM_INT);
         $statement->bindValue(':sid', $sid, PDO::PARAM_INT);
-
-
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function updateTeacher($teacherid, $name, $username, $password, $classid, $endDate)
     {
         global $dbh;
-
         $sql = "UPDATE teachers 
         SET name = :name, username = :username, password = :password, classid = :classid, endDate = :endDate 
         WHERE teacherid = :teacherid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':teacherid', $teacherid, PDO::PARAM_STR);
         $statement->bindValue(':name', $name, PDO::PARAM_STR);
         $statement->bindValue(':username', $username, PDO::PARAM_STR);
         $statement->bindValue(':password', $password, PDO::PARAM_STR);
         $statement->bindValue(':endDate', $endDate, PDO::PARAM_STR);
         $statement->bindValue(':classid', $classid, PDO::PARAM_INT);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
-
     function updateClass($className, $classid)
     {
         global $dbh;
-
         $sql = "UPDATE classes 
         SET className = :className
         WHERE classid = :classid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':className', $className, PDO::PARAM_STR);
         $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function updateHelper($name, $classid, $helperid)
     {
         global $dbh;
-
         $sql = "UPDATE helpers 
                 SET name = :name, classid = :classid
                 WHERE helperid = :helperid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':name', $name, PDO::PARAM_STR);
         $statement->bindValue(':classid', $classid, PDO::PARAM_STR);
         $statement->bindValue(':helperid', $helperid, PDO::PARAM_STR);
-
-
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function deleteHelper($helperid)
     {
         global $dbh;
-
         $sql = "DELETE FROM helpers WHERE helperid = :helperid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':helperid', $helperid, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function deleteStudent($sid)
     {
         global $dbh;
-
         $sql = "DELETE FROM students WHERE sid = :sid";
-
         $statement = $dbh->prepare($sql);
         $statement->bindValue(':sid', $sid, PDO::PARAM_INT);
         $statement->execute();
-
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     function deleteTeacher($teacherid)
     {
         global $dbh;
-
         $sql = "DELETE FROM teachers WHERE teacherid = :teacherid";
-
         $statement = $dbh->prepare($sql);
         $statement->bindValue(':teacherid', $teacherid, PDO::PARAM_INT);
         $statement->execute();
-
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
     }
-
     public function checkLogin($username, $password)
     {
         global $dbh;
-
         $sql = "SELECT DATEDIFF(teachers.endDate ,CURRENT_DATE()) daysLeft, teachers.*, classes.className FROM teachers INNER JOIN classes
         ON teachers.classid = classes.classid WHERE username = :username AND password = :password;";
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':username', $username, PDO::PARAM_STR);
         $statement->bindValue(':password', $password, PDO::PARAM_STR);
-
         $statement->execute();
-
         $results = $statement->fetch(PDO::FETCH_ASSOC);
-
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
         return $results;
     }
-
     public function checkIfAttedanceTaken($date, $sid)
     {
         global $dbh;
-
         $sql = "SELECT * FROM attendance WHERE date = :date AND sid = :sid";
-
         $statement = $dbh->prepare($sql);
-
         $statement->bindValue(':date', $date, PDO::PARAM_STR);
         $statement->bindValue(':sid', $sid, PDO::PARAM_STR);
-
         $statement->execute();
         $arr = $statement->errorInfo();
         if (isset($arr[2])) {
             print_r($arr[2]);
         }
-
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
         print_r($results);
         return $results;
